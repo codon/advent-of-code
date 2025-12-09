85727fb53e11c4069764122e7de72daa130533aa
Ivan Heffner <iheffner@gmail.com>
2025-12-08 18:17:43 -0800

    Day 06 part 2

--- Day06/aoc.py
+++ Day06/aoc.py
@@ -15,6 +15,10 @@ args = parser.parse_args()
 
 input_file = f"input/{'test' if args.test_mode else 'input'}.txt"
 
+# too low:  10189959076443
+#           10189959087258
+# too high: 21858311480784
+
 def cephalomath(data: list[int], oper) -> int:
     total = 1 if (oper == "*" or oper == "/") else 0
 
@@ -54,45 +58,76 @@ def check_part_1(data: list[list[int]], opers: list[str]) -> int:
         print(f'{sum_totals = }')
     return sum_totals
 
+def check_part_2(data: list[str], opers: list[str]) -> int:
     if args.debug_mode:
+        print(f'{data = }')
         print(f'{opers = }')
-    for row in data:
-        if args.debug_mode:
-            print(f'{row = }')
-        for ii in range(n):
-            if opers[ii] == "*":
-                totals[ii] *= row[ii]
-            elif opers[ii] == "/":
-                totals[ii] /= row[ii]
-            elif opers[ii] == "+":
-                totals[ii] += row[ii]
-            elif opers[ii] == "-":
-               totals[ii] -= row[ii]
-        if args.debug_mode:
-            print(f'{totals = }')
 
-    sum_sum = sum(totals)
+    n = len(opers)
+    total = 0
+    m = len(data) // n
+
+    int_data = [None if _.replace(' ', '') == '' else int(_) for _ in data]
+
+    operands, ii = [], 0
+    for operand in int_data:
+        if operand is None:
+            if args.debug_mode:
+                print(f'[{ii}] {operands} {opers[ii]}')
+            value = cephalomath(operands, opers[ii])
+            operands = []
+            ii += 1
+            total += value
+            if args.debug_mode:
+                print(f'[{ii}] {value=} => {total}')
+        else:
+            operands.append(operand)
+
+    if len(operands) != 0: 
+        if args.debug_mode:
+            print(f'[{ii}] {operands} {opers[ii]}')
+        value = cephalomath(operands, opers[ii])
+        operands = []
+        ii += 1
+        total += value
+        if args.debug_mode:
+            print(f'[{ii}] {value=} => {total}')
+
     if args.debug_mode:
-        print(f'{sum_sum = }')
-    return sum_sum
+        print(f'{total = }')
+    return total
 
-def check_part_2(data: list[list[int]], opers: list[str]) -> int:
-    pass
-
-rows: list[list[int]] = []
+rows: list[str|list[int]] = []
 operators: list[str] = []
 checksum = 0
 with open(input_file, 'r') as data:
     for line in map(lambda _: _.rstrip(), data):
         if line[0] in "*/+-":
             operators = [_ for _ in line.split(" ") if _ != ""]
-            if args.debug_mode:
-                print(f'{operators = }')
-        else:
+            # if args.debug_mode:
+            #     print(f'{operators = }')
+        elif args.part == 1:
             row = [int(_) for _ in line.split(" ") if _ != ""]
             rows.append(row)
-            if args.debug_mode:
-                print(f'{row = }')
+            # if args.debug_mode:
+            #     print(f'{row = }')
+        else:
+            if (m := len(rows)) < (n := len(line)):
+                # if args.debug_mode:
+                #     print(f'{len(rows)=} < {len(line)=}')
+                rows.extend([""] * (n - m))
+                # if args.debug_mode:
+                #     print(f'{rows=}')
+
+            for ii in range(n):
+                rows[ii] = rows[ii]+line[ii]
+
+            # if args.debug_mode:
+            #     print(f'{rows= }')
+
+
+# if args.debug_mode:
+#     print(f'{rows = }')
 
 checksum = (check_part_1 if args.part == 1 else check_part_2)(rows, operators)
 
